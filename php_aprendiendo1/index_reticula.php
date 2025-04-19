c83ad6619b9dcadeca1ab66821ffbbaf852a94e0
Gerry CR <gerry.dev73@gmail.com>
2025-04-19 05:54:48 -0600

    volvemos a subir a este repo, recordar el otro

---
+++ php_aprendiendo1/index_reticula.php
@@ -0,0 +1,677 @@
+<html>
+
+
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Mi Avance Reticular</title>
+	<!-- <style rel="stylesheet" href="styles\\styles1.css"></style> -- esta manera esta mal obviamente -->
+	<link rel="stylesheet" href="styles\\styles2.css">
+  <title>Aprendiendo HTML</title>
+
+</head>
+
+<body>
+	<h1>Hola mundo desde Nodepad++ en html</h1>
+	
+	
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="academicGrid"><div class="g-head">
+				<div class="g-col" style="width: 11.1111%;"><p>Grado 1</p></div>
+				<div class="g-col" style="width: 11.1111%;"><p>Grado 2</p></div>
+				<div class="g-col" style="width: 11.1111%;"><p>Grado 3</p></div>
+				<div class="g-col" style="width: 11.1111%;"><p>Grado 4</p></div>
+				<div class="g-col" style="width: 11.1111%;"><p>Grado 5</p></div>
+				<div class="g-col" style="width: 11.1111%;"><p>Grado 6</p></div>
+				<div class="g-col" style="width: 11.1111%;"><p>Grado 7</p></div>
+				<div class="g-col" style="width: 11.1111%;"><p>Grado 8</p></div>
+				<div class="g-col" style="width: 11.1111%;"><p>Grado 9</p></div>
+			</div>
+			
+			<div class="g-col g-col-1" style="width: 11.1111%;">
+				<div class="g-row green">
+					<p><span>ACF - 0901</span></p>
+					<p title="Calculo Diferencial"><small>Calculo Diferencial</small></p>
+					<p><span>74 / R1</span></p></div>
+				<div class="g-row green">
+					<p><span>AED-1285</span></p>
+					<p title="Fundamentos De Programacion"><small>Fundamentos De Programacion</small></p>
+					<p><span>75 / R1</span></p></div><div class="g-row green"><p><span>ACA-0907</span></p>
+					<p title="Taller De Etica"><small>Taller De Etica</small></p><p><span>75 / R1</span></p>
+				</div>
+				<div class="g-row green">
+					<p><span>AEF-1041</span></p>
+					<p title="Matematicas Discretas"><small>Matematicas Discretas</small></p>
+					<p><span>80 / O2</span></p>
+        </div>
+        <div class="g-row green">
+          <p><span>SCH-1024</span></p>
+          <p title="Taller De Administracion"><small>Taller De Administracion</small></p>
+          <p><span>81 / O1</span></p>
+        </div>
+        <div class="g-row green">
+          <p><span>ACC-0906</span></p>
+          <p title="Fundamentos De Investigacion"><small>Fundamentos De Investigacion</small></p>
+          <p><span>77 / O2</span></p>
+        </div><div class="g-row blue">
+          <p><span>B1L7</span></p>
+          <p title="Tutoria"><small>Tutoria</small></p>
+        </div><div class="g-row blue">
+          <p><span>B1L8</span></p>
+          <p title="Actividades Fisicas Para La Salud Y La Prevencion"><small>Actividades Fisicas Para La Salud Y La Prevencion</small></p>
+        </div>
+        <div class="g-row blue">
+            <p>
+              <span>ACF-2301</span>
+            </p>
+            <p title="Calculo Diferencial Nueva">
+              <small>Calculo Diferencial Nueva</small>
+            </p>
+          </div>
+
+          <div class="g-row blue">
+            <p>
+              <span>ACH-2307</span>
+            </p>
+            <p title="Taller De Etica Nueva">
+              <small>Taller De Etica Nueva</small>
+            </p>
+          </div>
+
+          </div>
+
+
+
+          <div class="g-col g-col-2" style="width: 11.1111%;">
+            <div class="g-row green">
+              <p>
+                <span>ACF-0902</span>
+              </p>
+              <p title="Calculo Integral">
+                <small>Calculo Integral</small>
+              </p>
+              <p>
+                <span>100 / EC</span>
+              </p>
+            </div>
+
+            <div class="g-row green">
+              <p>
+                <span>AED-1286</span>
+              </p>
+              <p title="Programacion Orientada A Objetos">
+                <small>Programacion Orientada A Objetos</small>
+              </p>
+              <p>
+                <span>77 / EC</span>
+              </p>
+            </div>
+
+            <div class="g-row green">
+              <p>
+                <span>AEC-1008</span>
+              </p>
+              <p title="Contabilidad Financiera">
+                <small>Contabilidad Financiera</small>
+              </p>
+              <p>
+                <span>91 / EC</span>
+              </p>
+            </div>
+
+            <div class="g-row green">
+              <p>
+                <span>AEC-1058</span>
+              </p>
+              <p title="Quimica">
+                <small>Quimica</small>
+              </p>
+              <p>
+                <span>91 / EC</span>
+              </p>
+            </div>
+
+            <div class="g-row green">
+              <p>
+                <span>ACF-0903</span>
+              </p>
+              <p title="Algebra Lineal">
+                <small>Algebra Lineal</small>
+              </p>
+              <p>
+                <span>97 / EC</span>
+              </p>
+            </div>
+
+            <div class="g-row blue">
+              <p>
+                <span>AEF-1052</span>
+              </p>
+              <p title="Probabilidad Y Estadistica">
+                <small>Probabilidad Y Estadistica</small>
+              </p>
+            </div>
+
+            <div class="g-row blue">
+              <p>
+                <span>B2L7</span>
+              </p>
+              <p title="Apreciacion De Las Artes Y Diversidad Cultural">
+                <small>Apreciacion De Las Artes Y Diversidad Cultural</small>
+              </p>
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+          </div>
+
+          <div class="g-col g-col-3" style="width: 11.1111%;">
+            <div class="g-row purple">
+              <p>
+                <span>ACF-0904</span>
+              </p>
+              <p title="Calculo Vectorial">
+                <small>Calculo Vectorial</small>
+              </p>
+              <p>
+                <span>Cursando</span>
+              </p>
+            </div>
+
+            <div class="g-row green">
+              <p>
+                <span>AED-1026</span>
+              </p>
+              <p title="Estructura De Datos">
+                <small>Estructura De Datos</small>
+              </p>
+              <p>
+                <span>90 / O1</span>
+              </p>
+            </div>
+
+            <div class="g-row green">
+              <p>
+                <span>SCC-1005</span>
+              </p>
+              <p title="Cultura Empresarial">
+                <small>Cultura Empresarial</small>
+              </p>
+              <p>
+                <span>100 / O1</span>
+              </p>
+            </div>
+
+            <div class="g-row blue">
+              <p>
+                <span>SCC-1013</span>
+              </p>
+              <p title="Investigacion De Operaciones">
+                <small>Investigacion De Operaciones</small>
+              </p>
+            </div>
+
+            <div class="g-row green">
+              <p>
+                <span>ACD-0908</span>
+              </p>
+              <p title="Desarrollo Sustentable">
+                <small>Desarrollo Sustentable</small>
+              </p>
+              <p>
+                <span>72 / R1</span>
+              </p>
+            </div>
+
+            <div class="g-row purple">
+              <p>
+                <span>SCF-1006</span>
+              </p>
+              <p title="Fisica General">
+                <small>Fisica General</small>
+              </p>
+              <p>
+                <span>Cursando</span>
+              </p>
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+          </div>
+
+          <div class="g-col g-col-4" style="width: 11.1111%;">
+            <div class="g-row blue">
+              <p>
+                <span>ACF-0905</span>
+              </p>
+              <p title="Ecuaciones Diferenciales">
+                <small>Ecuaciones Diferenciales</small>
+              </p>
+            </div>
+
+            <div class="g-row blue">
+              <p>
+                <span>SCC-1017</span>
+              </p>
+              <p title="Metodos Numericos">
+                <small>Metodos Numericos</small>
+              </p>
+            </div>
+
+            <div class="g-row blue">
+              <p>
+                <span>SCD-1027</span>
+              </p>
+              <p title="Topicos Avanzados De Programacion">
+                <small>Topicos Avanzados De Programacion</small>
+              </p>
+            </div>
+
+            <div class="g-row pink">
+              <p>
+                <span>AEF-1031</span>
+              </p>
+              <p title="Fundamentos De Base De Datos">
+                <small>Fundamentos De Base De Datos</small>
+              </p>
+              <p>
+                <span>Cursando</span>
+              </p>
+            </div>
+
+            <div class="g-row green">
+              <p>
+                <span>SCD-1022</span>
+              </p>
+              <p title="Simulacion">
+                <small>Simulacion</small>
+              </p>
+              <p>
+                <span>78 / O1</span>
+              </p>
+            </div>
+
+            <div class="g-row green">
+              <p>
+                <span>SCD-1018</span>
+              </p>
+              <p title="Principios Electricos Y Aplicaciones Digitales">
+                <small>Principios Electricos Y Aplicaciones Digitales</small>
+              </p>
+              <p>
+                <span>100 / O1</span>
+              </p>
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+          </div>
+
+          <div class="g-col g-col-5" style="width: 11.1111%;">
+            <div class="g-row green">
+              <p>
+                <span>SCC-1010</span>
+              </p>
+              <p title="Graficacion">
+                <small>Graficacion</small>
+              </p>
+              <p>
+                <span>99 / O1</span>
+              </p>
+            </div>
+
+            <div class="g-row green">
+              <p>
+                <span>AEC-1034</span>
+              </p>
+              <p title="Fundamentos De Telecomunicaciones">
+                <small>Fundamentos De Telecomunicaciones</small>
+              </p>
+              <p>
+                <span>100 / O1</span>
+              </p>
+            </div>
+
+            <div class="g-row purple">
+              <p>
+                <span>AEC-1061</span>
+              </p>
+              <p title="Sistemas Operativos">
+                <small>Sistemas Operativos</small>
+              </p>
+              <p>
+                <span>Cursando</span>
+              </p>
+            </div>
+
+            <div class="g-row blue">
+              <p>
+                <span>SCA-1025</span>
+              </p>
+              <p title="Taller De Base De Datos">
+                <small>Taller De Base De Datos</small>
+              </p>
+            </div>
+
+            <div class="g-row green">
+              <p>
+                <span>SCC-1007</span>
+              </p>
+              <p title="Fundamentos De Ingenieria De Software">
+                <small>Fundamentos De Ingenieria De Software</small>
+              </p>
+              <p>
+                <span>85 / O1</span>
+              </p>
+            </div>
+
+            <div class="g-row purple">
+              <p>
+                <span>SCD-1003</span>
+              </p>
+              <p title="Arquitectura De Computadoras">
+                <small>Arquitectura De Computadoras</small>
+              </p>
+              <p>
+                <span>Cursando</span>
+              </p>
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+          </div>
+
+          <div class="g-col g-col-6" style="width: 11.1111%;">
+            <div class="g-row blue">
+              <p>
+                <span>SCC-1014</span>
+              </p>
+              <p title="Lenguajes De Interfaz">
+                <small>Lenguajes De Interfaz</small>
+              </p>
+            </div>
+
+            <div class="g-row purple">
+              <p>
+                <span>SCD-1021</span>
+              </p>
+              <p title="Redes De Computadoras">
+                <small>Redes De Computadoras</small>
+              </p>
+              <p>
+                <span>Cursando</span>
+              </p>
+            </div>
+
+            <div class="g-row blue">
+              <p>
+                <span>SCA-1026</span>
+              </p>
+              <p title="Taller De Sistemas Operativos">
+                <small>Taller De Sistemas Operativos</small>
+              </p>
+            </div>
+
+            <div class="g-row blue">
+              <p>
+                <span>SCB-1001</span>
+              </p>
+              <p title="Administracion De Base De Datos">
+                <small>Administracion De Base De Datos</small>
+              </p>
+            </div>
+
+            <div class="g-row blue">
+              <p>
+                <span>SCD-1011</span>
+              </p>
+              <p title="Ingenieria De Software">
+                <small>Ingenieria De Software</small>
+              </p>
+            </div>
+
+            <div class="g-row purple">
+              <p>
+                <span>AEB-1055</span>
+              </p>
+              <p title="Programacion Web">
+                <small>Programacion Web</small>
+              </p>
+              <p>
+                <span>Cursando</span>
+              </p>
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+          </div>
+
+          <div class="g-col g-col-7" style="width: 11.1111%;">
+            <div class="g-row blue">
+              <p>
+                <span>SCD-1015</span>
+              </p>
+              <p title="Lenguajes Y Automatas I">
+                <small>Lenguajes Y Automatas I</small>
+              </p>
+            </div>
+
+            <div class="g-row blue">
+              <p>
+                <span>SCD-1004</span>
+              </p>
+              <p title="Conmutacion Y Enrutamiento En Redes De Datos">
+                <small>Conmutacion Y Enrutamiento En Redes De Datos</small>
+              </p>
+            </div>
+
+            <div class="g-row blue">
+              <p>
+                <span>ACA-0909</span>
+              </p>
+              <p title="Taller De Investigacion I">
+                <small>Taller De Investigacion I</small>
+              </p>
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row blue">
+              <p>
+                <span>SCG-1009</span>
+              </p>
+              <p title="Gestion De Proyectos De Software">
+                <small>Gestion De Proyectos De Software</small>
+              </p>
+            </div>
+
+            <div class="g-row blue">
+              <p>
+                <span>SCC-1023</span>
+              </p>
+              <p title="Sistemas Programables">
+                <small>Sistemas Programables</small>
+              </p>
+            </div>
+
+            <div class="g-row blue">
+              <p>
+                <span>B9L4</span>
+              </p>
+              <p title="Actividades Complementarias">
+                <small>Actividades Complementarias</small>
+              </p>
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+          </div>
+
+          <div class="g-col g-col-8" style="width: 11.1111%;">
+            <div class="g-row grey">
+              <p>
+                <span>SCD-1016</span>
+              </p>
+              <p title="Lenguajes Y Automatas II">
+                <small>Lenguajes Y Automatas II</small>
+              </p>
+            </div>
+
+            <div class="g-row grey">
+              <p>
+                <span>SCA-1002</span>
+              </p>
+              <p title="Administracion De Redes">
+                <small>Administracion De Redes</small>
+              </p>
+            </div>
+
+            <div class="g-row grey">
+              <p>
+                <span>ACA-0910</span>
+              </p>
+              <p title="Taller De Investigacion II">
+                <small>Taller De Investigacion II</small>
+              </p>
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row grey">
+              <p>
+                <span>SCC-1019</span>
+              </p>
+              <p title="Programacion Logica Y Funcional">
+                <small>Programacion Logica Y Funcional</small>
+              </p>
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row grey">
+              <p>
+                <span>B9L3</span>
+              </p>
+              <p title="Servicio Social">
+                <small>Servicio Social</small>
+              </p>
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+          </div>
+
+          <div class="g-col g-col-9" style="width: 11.1111%;">
+            <div class="g-row grey">
+              <p>
+                <span>SCC-1012</span>
+              </p>
+              <p title="Inteligencia Artificial">
+                <small>Inteligencia Artificial</small>
+              </p>
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row grey">
+              <p>
+                <span>B9L2</span>
+              </p>
+              <p title="Residencia Profesional">
+                <small>Residencia Profesional</small>
+              </p>
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+            </div>
+
+            <div class="g-row empty">
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</div>
+
+
+</body>
+
+
+</html>
