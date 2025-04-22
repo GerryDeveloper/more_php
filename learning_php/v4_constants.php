76b4eabe383affbf62ed653ea6ef17dd621c7ad3
Gerry CR <gerry.dev73@gmail.com>
2025-04-22 07:40:23 -0600

    constantes, variables, variables constantes

--- learning_php/v4_constants.php
+++ learning_php/v4_constants.php
@@ -41,6 +41,85 @@ echo geth5('define() function');
 echo '- constants created with the define() function are defined at runtime';
 echo '- you CAN define constans using the define() function within a control structure';
 
+echo "<br>";
+echo 'Ejemplo de definicion de constante usando const:<br>conts CONST1 = "sator"';
+const CONST1 = 'sator';
+// esto no funciona porque PHP intepreta el contenido con $ como una variable, no lo es
+// echo "valor de la constante: CONST1 = {$CONST1}";
+echo "valor de la constante CONST1 = " . CONST1; // esta es la forma mas clara y comun de mostrar el valor de una var
+
+br();
+echo geth3('Constantes con "const" y "define()" dentro de estructuras de control, if-else, loops...');
+
+echo getp('- constants created with "const" keyword are defined at COMPILE TIME
+  <br>- while constnts created with the "define()" function are defined at RUNTIME
+  <br>constants with const keyword cannot be defined within your control structures like if-else, loops.');
+
+echo geth4 ("definiendo una constante usando define() dentro de una estructura de control");
+echo "<br>" . getp ('if (true) {<br>
+    define("STATUS_PAID", 9);<br>
+  }<br>');
+
+if (true) {
+  define('STATUS_PAID2', 9);
+}
+// imprimimos la constante definida en tiempo de RUNTIME
+echo STATUS_PAID2;
+
+br();
+echo geth3 ("Puedes usar \$variables para definir tus nombres de constantes");
+
+$paid = 'PAID3';
+$paidValue = 66;
+
+define ('STATUS_' . $paid, $paidValue);
+
+echo '<br>' . STATUS_PAID3; 
+
+br();
+echo geth3 ('Predefined CONSTANTS');
+echo "- php version: ";
+echo "<br>" . PHP_VERSION;
+echo "<br>- php mayor version: " .  PHP_MAJOR_VERSION;
+
+br();
+echo geth3 ('MAGIC CONSTANTS');
+echo '- It\'s value change dependind on where its used';
+echo '<br>- are defined in COMPILE TIME, normally constants are defined at RUNTIME';
+echo '<br>- example: __LINE__, return the line of wher is it';
+echo '<br>' . __LINE__;
+echo '<br>' . __LINE__;
+echo '<br>' . __LINE__;
+
+br();
+echo geth1 ('Variable Variables');
+echo '- takes the valur of the variable and treats that as the name of the new variable';
+echo '<br>- toma el valor de la variable y lo usa como nomobre';
+
+$foo = 'bar';
+
+$$foo = 'baz'; // toma el valor de $foo, que es 'bar', y ahora ese es el nombre de esta variable, cuyo valor es 'baz'
+
+echo '<br>';
+echo $foo; // bar
+echo '<br>';
+echo $$foo; // baz
+echo "<br>";
+echo $bar; //baz
+echo '<br><br>';
+var_dump ($foo);
+echo '<br>';
+var_dump ($$foo);
+echo '<br>';
+var_dump ($bar);
+echo '<br><br>';
+
+echo "$foo, $$foo<br>"; // this will not work for the variable variable
+echo "$foo, {$$foo}<br>"; // do this or bellow instead
+echo "$foo, ${$foo}<br>"; 
+
+
+
 // functions
 function br($quantity = 1) {
   for ($i = 0; $i < $quantity; $i ++) {
@@ -49,6 +128,12 @@ function br($quantity = 1) {
   echo '<hr>';
 }
 
+function geth1 ($arg) {
+  return "<h1>$arg</h1>";
+}
+function geth2 ($arg) {
+  return "<h2>$arg</h2>";
+}
 function geth3 ($arg) {
   return "<h3>$arg</h3>";
 }
@@ -57,4 +142,8 @@ function geth4 ($arg) {
 }
 function geth5 ($arg) {
   return "<h5>$arg</h4>";
+}
+
+function getp ($arg) {
+  return "<p>$arg</p>";
 }
